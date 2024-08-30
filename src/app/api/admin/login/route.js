708791b10fbe073801f9cbf39f8116c5eefcd8be
Murtaza Patel <imurtaza97@gmail.com>
2024-08-30 22:45:54 +0530

    ip removed

--- src/app/api/admin/login/route.js
+++ src/app/api/admin/login/route.js
@@ -5,18 +5,6 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import cookie from 'cookie';
 
-function normalizeIP(ip) {
-  // Handle IPv4-mapped IPv6 addresses
-  if (ip.startsWith('::ffff:')) {
-    return ip.substring(7);
-  }
-  // Handle IPv6 loopback address
-  if (ip === '::1') {
-    return '127.0.0.1';
-  }
-  return ip;
-}
-
 export async function POST(req) {
   try {
     await dbConnect();
@@ -29,10 +17,6 @@ export async function POST(req) {
       );
     }
 
-    // Get IP address from request headers
-    const rawIpAddress = req.headers.get('x-forwarded-for') || req.headers.get('remote-addr') || 'Unknown IP';
-    const ipAddress = normalizeIP(rawIpAddress);
-
     // Find the admin by email
     const admin = await Admin.findOne({ email });
     if (!admin) {
@@ -42,14 +26,6 @@ export async function POST(req) {
       );
     }
 
-    // Check if IP address is correct
-    const normalizedAdminIp = normalizeIP(admin.ipAddress);
-    if (normalizedAdminIp !== ipAddress) {
-      return NextResponse.json(
-        { error: 'Invalid IP address.' },
-        { status: 401 }
-      );
-    }
 
     // Check if password is correct
     const isMatch = await bcrypt.compare(password, admin.password);
@@ -68,12 +44,12 @@ export async function POST(req) {
     );
 
     // Set cookie with token
-    const serializedCookie = cookie.serialize('token',token, {
+    const serializedCookie = cookie.serialize('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      maxAge: 12 * 3600, // 12 hour
+      maxAge: 12 * 3600, // 12 hours
       path: '/',
-      sameSite: 'Strict', 
+      sameSite: 'Strict',
     });
 
     const response = NextResponse.json(
@@ -84,6 +60,7 @@ export async function POST(req) {
     response.headers.append('Set-Cookie', serializedCookie);
     return response;
   } catch (error) {
+    console.error('Error during login:', error); // Add detailed logging
     return NextResponse.json(
       { error: 'An error occurred while logging in.' },
       { status: 500 }
