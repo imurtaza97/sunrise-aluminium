9ffa028e85b561c81b02dc110d8aed3fdd8c6b42
Murtaza Patel <imurtaza97@gmail.com>
2024-09-01 21:56:15 +0530

    analytics charts

--- src/app/api/ga-data/route.js
+++ src/app/api/ga-data/route.js
@@ -3,12 +3,14 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
     try {
+        const privateKey = process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n');
+        
         const auth = new google.auth.GoogleAuth({
             credentials: {
                 type: 'service_account',
                 project_id: process.env.GOOGLE_PROJECT_ID,
                 private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
-                private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+                private_key: privateKey,
                 client_email: process.env.GOOGLE_CLIENT_EMAIL,
                 client_id: process.env.GOOGLE_CLIENT_ID,
                 auth_uri: process.env.GOOGLE_AUTH_URI,
